580582eb8902af1b2ccc9f4d389197524347014c
Enrique Urbina <btace1347@yahoo.com>
2016-12-08 14:41:27 -0600

    initial commit

---
+++ BudgetApp Attempt/BudgetApp Attempt/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace BudgetApp_Attempt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.incomeTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rentTextBox = new System.Windows.Forms.TextBox();
+            this.carTextBox = new System.Windows.Forms.TextBox();
+            this.foodTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.rentPerTextBox = new System.Windows.Forms.TextBox();
+            this.anualTextBox = new System.Windows.Forms.TextBox();
+            this.foodPerTextBox = new System.Windows.Forms.TextBox();
+            this.carPerTextBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.savingsTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "The Budget App";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Your Total Income";
+            // 
+            // incomeTextBox
+            // 
+            this.incomeTextBox.Location = new System.Drawing.Point(120, 133);
+            this.incomeTextBox.Name = "incomeTextBox";
+            this.incomeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.incomeTextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(317, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Your Expenses (Monthly)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Rent";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 381);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Food";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 341);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Car Payment";
+            // 
+            // rentTextBox
+            // 
+            this.rentTextBox.Location = new System.Drawing.Point(141, 300);
+            this.rentTextBox.Name = "rentTextBox";
+            this.rentTextBox.Size = new System.Drawing.Size(100, 20);
+            this.rentTextBox.TabIndex = 7;
+            // 
+            // carTextBox
+            // 
+            this.carTextBox.Location = new System.Drawing.Point(141, 338);
+            this.carTextBox.Name = "carTextBox";
+            this.carTextBox.Size = new System.Drawing.Size(100, 20);
+            this.carTextBox.TabIndex = 8;
+            // 
+            // foodTextBox
+            // 
+            this.foodTextBox.Location = new System.Drawing.Point(141, 378);
+            this.foodTextBox.Name = "foodTextBox";
+            this.foodTextBox.Size = new System.Drawing.Size(100, 20);
+            this.foodTextBox.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(433, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(296, 31);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Percentages (Annually)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(804, 217);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(111, 31);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Savings";
+            // 
+            // rentPerTextBox
+            // 
+            this.rentPerTextBox.Location = new System.Drawing.Point(439, 303);
+            this.rentPerTextBox.Name = "rentPerTextBox";
+            this.rentPerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.rentPerTextBox.TabIndex = 12;
+            // 
+            // anualTextBox
+            // 
+            this.anualTextBox.Location = new System.Drawing.Point(810, 303);
+            this.anualTextBox.Name = "anualTextBox";
+            this.anualTextBox.Size = new System.Drawing.Size(100, 20);
+            this.anualTextBox.TabIndex = 13;
+            // 
+            // foodPerTextBox
+            // 
+            this.foodPerTextBox.Location = new System.Drawing.Point(439, 374);
+            this.foodPerTextBox.Name = "foodPerTextBox";
+            this.foodPerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.foodPerTextBox.TabIndex = 16;
+            // 
+            // carPerTextBox
+            // 
+            this.carPerTextBox.Location = new System.Drawing.Point(439, 334);
+            this.carPerTextBox.Name = "carPerTextBox";
+            this.carPerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.carPerTextBox.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(838, 284);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Annually";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(547, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 43);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Calculate!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(19, 417);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(45, 13);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Savings";
+            // 
+            // savingsTextBox
+            // 
+            this.savingsTextBox.Location = new System.Drawing.Point(141, 417);
+            this.savingsTextBox.Name = "savingsTextBox";
+            this.savingsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.savingsTextBox.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 580);
+            this.Controls.Add(this.savingsTextBox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.carPerTextBox);
+            this.Controls.Add(this.foodPerTextBox);
+            this.Controls.Add(this.anualTextBox);
+            this.Controls.Add(this.rentPerTextBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.foodTextBox);
+            this.Controls.Add(this.carTextBox);
+            this.Controls.Add(this.rentTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.incomeTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox incomeTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox rentTextBox;
+        private System.Windows.Forms.TextBox carTextBox;
+        private System.Windows.Forms.TextBox foodTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox rentPerTextBox;
+        private System.Windows.Forms.TextBox anualTextBox;
+        private System.Windows.Forms.TextBox foodPerTextBox;
+        private System.Windows.Forms.TextBox carPerTextBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox savingsTextBox;
+    }
+}
+
---
+++ BudgetApp Attempt/BudgetApp Attempt/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+//ToDO: learn to save to a Database
+
+
+
+
+namespace BudgetApp_Attempt
+{
+    public partial class Form1 : Form
+    {
+
+                
+            
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // To Get Percentage Formula:
+            // multiply "rent" by 100 then divide that by income
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+   
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double income = Convert.ToInt32(incomeTextBox.Text);
+            double rent = Convert.ToInt32(rentTextBox.Text);
+            double car = Convert.ToInt32(carTextBox.Text);
+            double food = Convert.ToInt32(foodTextBox.Text);
+            double savings = Convert.ToInt32(savingsTextBox.Text);
+
+
+            //Test
+
+            rentPerTextBox.Text = Convert.ToString(Math.Round(rent * 12 * 100 / income)+"%");
+            carPerTextBox.Text = Convert.ToString(Math.Round(car * 12 * 100 / income) + "%");
+            foodPerTextBox.Text = Convert.ToString(Math.Round(food * 12 * 100 / income) + "%");
+            anualTextBox.Text = Convert.ToString("$" + savings * 12);
+
+
+
+
+
+        }
+    }
+}
